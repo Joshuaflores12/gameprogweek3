05960dbd19205e9612d14ddd885a3e7274e8a405
Joshua Gabriel L. Flores <joshuagabriellflores@gmail.com>
2023-09-08 13:38:36 +0800

    week 3
    game prog 2

---
+++ Assets/script/mesh renderer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class meshrenderer : MonoBehaviour
+{
+    public Renderer rend;  // variable for renderer
+    public Color [] colors; // colors variable array
+    public float speed;
+    public Transform pointA, pointB;
+    public float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>(); // calls the mesh renderer
+        // transform.position = Vector3.down; //game object model position x,y,z
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.back * speed * Time.deltaTime); // game object movement  
+        //transform.position = Vector3.Lerp(pointA.position,pointB.position, speed * Time.time); // makes the game object moves from point a to b
+        //transform.position = Vector3.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime); // makes the game object moves towards a specific position
+        float dist = Vector3.Distance(transform.position,pointA.position); // distance between player and point a
+        Debug.Log(dist + "enemy has entered");
+    
+    }
+
+    private void OnEnable()
+    {
+        // rend.material.color = colors[Random.Range(0,colors.Length)]; // selects the number of range based on how many colors are presents in the lists
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        // rend.material.color = colors[Random.Range(0, colors.Length)]; // selects the number of range based on how many colors are presents in the lists
+    }
+
+    private void OnMouseEnter()
+    {
+        // rend.material.color = colors[Random.Range(0, colors.Length)]; // selects the number of range based on how many colors are presents in the lists
+    }
+
+    private void OnMouseExit()
+    {
+        // rend.material.color = colors[Random.Range(0, colors.Length)]; // selects the number of range based on how many colors are presents in the lists
+    }
+
+    private void OnMouseUp()
+    {
+        // rend.material.color = colors[Random.Range(0, colors.Length)]; //selects the number of range based on how many colors are presents in the lists
+    }
+
+    private void OnMouseDrag()
+    {
+        rend.material.color = colors[Random.Range(0, colors.Length)]; //selects the number of range based on how many colors are presents in the lists
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+}
